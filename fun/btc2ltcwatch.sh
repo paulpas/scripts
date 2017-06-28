f7194086849a87a8576885af0ab15b6db869b367
Paul Pasika <paul.pasika@ge.com>
2017-06-28 14:02:50 -0500

    Changed to ETH and LTC and adjusted for website format changes

--- fun/btc2ltcwatch.sh
+++ fun/btc2ltcwatch.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-array=( $( for i in btc ltc
+array=( $( for i in eth ltc
 do
-curl -A Mozilla https://btc-e.com/exchange/"$i"_usd 2>&1| awk '/Last Price/{print $4}' | awk -F\> '{print $2}'
+curl -A Mozilla https://btc-e.com/exchange/"$i"_usd 2>&1| awk '/Last: /{print $3}' | awk -F\> '{print $2}'
 done) )
 delta=`echo ${array[0]} - ${array[1]} | bc -l`
-percent=`echo ${array[1]} / ${array[0]} | bc -l`
+percent=`echo "scale=4; ${array[1]} / ${array[0]} *100" | bc -l`
 echo ${array[*]} $delta $percent%

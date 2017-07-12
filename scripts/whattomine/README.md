9ad903c40fe641548ff5cb4fc1b542c9d7bcd720
Paul Pasika <paulpas@petabit.net>
2017-07-12 13:54:37 -0500

    added miner switching script and a README

---
+++ scripts/whattomine/README.md
@@ -0,0 +1,29 @@
+Auto switch between the most profitable coin.
+
+It assumes that you have set up systemd for claymore on Ubuntu:
+
+vi /etc/systemd/system/claymore.service
+
+cat << EOF > /tmp/claymore.service
+[Unit]
+Description=Claymore Etherenet Miner
+After=syslog.target
+
+[Service]
+Type=simple
+User=paulpas
+PIDFile=/var/pid/claymore.pid
+ExecStartPre=/bin/rm -f /var/run/claymore.pid
+ExecStart=/opt/Claymore/start.bash
+Restart=on-abort
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl daemon-reload
+systemctl enable claymore.service
+systemctl start claymore.service
+
+
+On ethOS is requires that you have your claymore miner utilizing claymore.stub.conf
---
+++ scripts/whattomine/switchclaymore.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# Detect OS, Ubuntu or ethos
+# I install Claymore in /opt/Claymore on Ubuntu
+
+if uname -r | grep -- "-generic"
+then
+        ClaymoreROOT=/opt/Claymore
+        ClaymoreRUN=$ClaymoreROOT/start.bash
+        RestartCMD="sudo systemctl restart claymore.service"
+elif uname -r | grep -- "-ethos"
+then
+        ClaymoreROOT=$HOME
+        ClaymoreRUN=$ClaymoreROOT/claymore.stub.conf
+        RestartCMD="minestop; sleep 2; minestart"
+fi
+
+# Collect the most profitable coin, currently only ETH and ETC are setup.
+BestCoin=`pullstats.jq.sh | grep -E 'ET[CH]' | head -1 | awk '{print $1}' | tr '[:upper:]' '[:lower:]'`
+
+# move config file to the most profitable coin
+cp -f $ClaymoreRUN.$BestCoin $ClaymoreRUN
+
+# restart miner
+echo $RestartCMD | sh
+

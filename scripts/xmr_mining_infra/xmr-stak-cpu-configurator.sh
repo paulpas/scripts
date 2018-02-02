0f5130681b9dc799bfda234468b16b8a53d56f53
paulpas <paulpas@petabit.net>
2018-02-01 21:41:35 -0600

    First run, assumed you're using DRBL

--- scripts/xmr_mining_infra/xmr-stak-cpu-configurator.sh
+++ scripts/xmr_mining_infra/xmr-stak-cpu-configurator.sh
@@ -33,19 +33,19 @@ $XMR_CPU_MINER $ConfigSrc | sed -e '1,/BEGIN/d' -e '/END/,$d'  >> $ConfigDest
 sed -i "s/WORKERID/$WorkerID/" $ConfigDest
 
 # Disable HT
-for CPU in /sys/devices/system/cpu/cpu[0-9]*; do
-    CPUID=`basename $CPU | cut -b4-`
-    echo -en "CPU: $CPUID\t"
-    [ -e $CPU/online ] && echo "1" > $CPU/online
-    THREAD1=`cat $CPU/topology/thread_siblings_list | cut -f1 -d,`
-    if [ $CPUID = $THREAD1 ]; then
-        echo "-> enable"
-        [ -e $CPU/online ] && echo "1" > $CPU/online
-    else
-        echo "-> disable"
-        echo "0" > $CPU/online
-    fi
-done
+#for CPU in /sys/devices/system/cpu/cpu[0-9]*; do
+#    CPUID=`basename $CPU | cut -b4-`
+#    echo -en "CPU: $CPUID\t"
+#    [ -e $CPU/online ] && echo "1" > $CPU/online
+#    THREAD1=`cat $CPU/topology/thread_siblings_list | cut -f1 -d,`
+#    if [ $CPUID = $THREAD1 ]; then
+#        echo "-> enable"
+#        [ -e $CPU/online ] && echo "1" > $CPU/online
+#    else
+#        echo "-> disable"
+#        echo "0" > $CPU/online
+#    fi
+#done
 
 $XMR_CPU_MINER $ConfigDest
 

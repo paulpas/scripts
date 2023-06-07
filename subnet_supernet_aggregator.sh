70ae4e17cda6050dcf79d94ba461a5b9ca5fa885
Paul <paulpas@petabit.net>
2023-06-07 11:52:57 -0500

    subnet_supernet_aggregator.sh

--- subnet_supernet_aggregator.sh
+++ subnet_supernet_aggregator.sh
@@ -3,4 +3,4 @@
 SUBNET=10.128.0.0
 SUPERNET_CIDR=10
 AGGREGATE_CIDR=19
-ipcalc -n ${SUBNET}/${SUPERNET_CIDR} -s $(seq $(echo "2^(32-${SUPERNET_CIDR})/2^(32-${AGGREGATE_CIDR})" | bc)  | xargs -I{} echo -n "$(echo "2^(32-${AGGREGATE_CIDR})-2" | bc) ") | awk '/^Network:/ {print $2}'
+ipcalc -n ${SUBNET}/${SUPERNET_CIDR} -s $(seq $(echo "2^(32-${SUPERNET_CIDR})/2^(32-${AGGREGATE_CIDR})" | bc)  | xargs -I{} echo -n "$(echo "2^(32-${AGGREGATE_CIDR})-2" | bc) ") | awk '/^Network:/ {print $2}' | grep -v "${SUBNET}/${SUPERNET_CIDR}"
